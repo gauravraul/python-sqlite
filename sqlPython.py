278192cd63aa9f4d195a717b6dcc7a66d5b9ef52
Gaurav Raul <raulgaurav96@gmail.com>
2019-06-25 23:41:13 +0530

    Connected to sqlite db with python and created table 'store'

---
+++ sqlPython.py
@@ -0,0 +1,9 @@
+# Program to conect to SQL lite Database using python
+
+import sqlite3
+
+conn = sqlite3.connect("lite.db")
+cur = conn.cursor()
+cur.execute("CREATE TABLE store (item TEXT , quantity INTEGER, price REAL)")
+conn.commit()
+conn.close()
